6d718615e2ffb4bdfd8d55d7803ffbea74711711
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-16 15:16:12 +0100

    JSnack 1

--- JSnack1/myscript.js
+++ JSnack1/myscript.js
@@ -4,3 +4,28 @@
 //  all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 
 //Svolgimento
+
+const myArr = [];
+let add = 0;
+
+do{
+
+    let n = parseInt(prompt('Inserisci un Numero'));
+    add += n; 
+    myArr.push(add)
+
+}while(add < 50);
+
+console.log(add);
+
+
+
+
+
+
+
+
+
+
+
+
